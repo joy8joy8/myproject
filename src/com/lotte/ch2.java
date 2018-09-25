7cd867c89323c5b8773c1af3a1ba1a18b778b132
user <user@es302n29>
2018-09-25 11:50:22 +0800

    9/25上課

---
+++ src/com/lotte/ch2.java
@@ -0,0 +1,29 @@
+package com.lotte;
+
+public class ch2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 120;
+		String firstName = "charlotte";
+		String lastName = "Lin";
+		int hour = 11, minute = 12;
+		System.out.print("your age : ");
+		System.out.println(age);
+		System.out.print("congratulation");
+		System.out.print(" ");
+		System.out.print(firstName);
+		System.out.print(" ");
+		System.out.print(lastName);
+		System.out.print(" wish you another 120 happy years\nthe time now is ");
+		System.out.print(hour);
+		System.out.print(":");
+		System.out.println(minute);
+
+		float weight = 57.3f;
+		float weight2 = (float) 50.5;
+		System.out.print("keep going you still have 7kg to lost to reach your goal");
+		System.out.println(weight2);
+	}
+
+}
