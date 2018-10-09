b13753b5d1729b89450835023a9db9a94811f6d2
user <user@es302n23>
2018-10-09 12:00:39 +0800

    homework

---
+++ src/com/home/Score.java
@@ -0,0 +1,11 @@
+package com.home;
+
+public class Score {
+	float english;
+	float math;
+	String name;
+	public Score(String name,float english,float math){
+		this.name=name;
+		this.english=english;
+	}
+}
