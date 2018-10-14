4feb32e73012d90a0cf5764dd870a32f3e759095
USER <USER@LAPTOP-NP70I9FR>
2018-10-14 19:59:33 +0800

    kind of

--- src/com/home/Print.java
+++ src/com/home/Print.java
@@ -7,10 +7,24 @@ public class Print {
 		Score one = new Score(85,66);
 		Score two = new Score(95,98);
 		Score three = new Score(100,88);
-		Name j = new Name("Jack");
-		System.out.print(j.name);
+		Name onej = new Name("Jack");
+		Name twoc = new Name("Charlotte");
+		Name threek = new Name("Kate");
+		System.out.print(onej.name);
+		System.out.print(" ");
 		System.out.print(one.english);
-		System.out.print(one.math);
+		System.out.print(" ");
+		System.out.println(one.math);
+		System.out.print(twoc.name);
+		System.out.print(" ");
+		System.out.print(two.english);
+		System.out.print(" ");
+		System.out.println(two.math);
+		System.out.print(threek.name);
+		System.out.print(" ");
+		System.out.print(three.english);
+		System.out.print(" ");
+		System.out.println(three.math);
 	}
 
 }
