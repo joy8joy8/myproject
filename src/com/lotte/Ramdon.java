c3ee2e41daf492a48de2d0d7f33fd8a716967f50
USER <USER@LAPTOP-NP70I9FR>
2018-11-04 20:24:29 +0800

    random number for only one

--- src/com/lotte/Ramdon.java
+++ src/com/lotte/Ramdon.java
@@ -7,30 +7,25 @@ public class Ramdon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random random=new Random();
-		int secret=random.nextInt(100)+1; 
-		System.out.println(secret);
-		
+		Random random = new Random();
+		int secret = random.nextInt(100) + 1;
+		//System.out.println(secret);
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Please guess a number from 1 to 100."+"\n"+"Your number: ");
-		double g=scanner.nextDouble();
-		if(g>secret){
-			int side =g;			
-			System.out.print("Please guess a number from 1 to "+g+"\n"+"Your number: ");
-		}else if(g<secret){
-			System.out.print("Please guess a number from "+g+" to 100"+"\n"+"Your number: ");
+		System.out.println("Please guess a number from 1 to 100.");
+		while (true) {
+			System.out.print("Your number: ");
+			int g = scanner.nextInt();
+			scanner.nextLine();
+			if (g > secret) {
+				System.out.print("Please guess a number below " + g+"\n");
+			} else if (g < secret) {
+				System.out.print("Please guess a number over " + g+"\n");
+			}else {
+			System.out.println("Correct! Nice guess!");
+			break;
+			}
 		}
-		while(g!=secret){
-			double d=scanner.nextDouble();
-						
-		}
-		
-	/*	if(g==secret){
-			System.out.println("Bravo");
-		}else{
-			System.out.println("wrong guess");
-		}*/
-		
+
 	}
 
 }
