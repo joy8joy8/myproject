387ebdd26aef6d7723e525b943e14aa99b3e92b4
user <user@es302n11>
2018-11-13 11:57:39 +0800

    for and practiceQ

---
+++ src/com/lotte/For.java
@@ -0,0 +1,23 @@
+package com.lotte;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Scanner;
+
+import midtermexam.Exam3Random;
+
+public class For {
+
+	public static void main(String[] args) {
+		int o = 4;
+		for (o = 0; o <= 6; o++) {
+			System.out.println(o);
+		}
+		System.out.println("now " + o);
+	}
+//絤策
+//1~100 い┮Τ5计
+//瞦计程Ω
+//叫10顶糷
+	}
