42023f8c30e68e392c4741f1b5fa5301b2a8682e
Charlotte <43362998+joy8joy8@users.noreply.github.com>
2018-12-17 22:42:16 +0800

    ch9 practice

---
+++ src/com/home/ChNine.java
@@ -0,0 +1,26 @@
+package com.home;
+
+public class ChNine {
+
+	public static void main(String[] args) {
+		
+		for(char c='A';c<='Z';c++) {
+			System.out.print(c);
+			if(c%5==0) {
+				System.out.println();
+			}
+		}
+		
+		System.out.println("Greek");
+		for(int i=913;i<=937;i++) {
+			System.out.print((char)i);
+		}
+		
+		
+		for(int i=0;i< fruit.length();i++) {
+			char letter=fruit.charAt(i);
+			System.out.println(letter);
+		}
+	}
+
+}
