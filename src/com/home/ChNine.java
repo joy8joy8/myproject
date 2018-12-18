bcf70923ba8c058116c1e581302beed2c4120fe7
user <user@es302n11>
2018-12-18 12:01:12 +0800

    string class

--- src/com/home/ChNine.java
+++ src/com/home/ChNine.java
@@ -16,11 +16,44 @@ public class ChNine {
 			System.out.print((char)i);
 		}
 		
-		
+		System.out.println();
+		String fruit="Hello world";
 		for(int i=0;i< fruit.length();i++) {
 			char letter=fruit.charAt(i);
-			System.out.println(letter);
+			System.out.print(letter);
 		}
-	}
+		System.out.println();
+		for(int i=fruit.length()-1;i>=0;i--){
+			char letter=fruit.charAt(i);
+			System.out.print(letter);
+		}
+		
+		System.out.println();
+		System.out.println(fruit.substring(8));
+		System.out.println(fruit.substring(2,5));
+		
+		int index = fruit.indexOf('l',4);
+		System.out.println(index);
 
+	String s2="Jack Tom Eric";
+	System.out.println(s2.indexOf("Tom"));
+	System.out.println(s2.indexOf("Eric"));
+	System.out.println(s2.lastIndexOf("c"));
+	
+	String name="Tom";
+	System.out.println(s2.substring(0, s2.indexOf("Tom"))+"boss"
+						+s2.substring(s2.indexOf("Tom")+name.length(),s2.lastIndexOf("c")+1));
+	
+	//while loop
+	s2=s2.replace("Tom", "boss");
+	System.out.println(s2);
+	
+	int n =Integer.parseInt("12");
+	n=n+2;
+	System.out.println(n);
+	float g=Float.parseFloat("59.7");
+	char c= Character.
+	
+	
+	}
 }
